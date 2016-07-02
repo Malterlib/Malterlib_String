bced706dac1c97462861bc48b9bd57ffcc4753bb
Erik Olofsson <erik.olofsson@hansoft.com>
2016-07-02 18:00:42 +0000

    [Integrate] //Malterlib/Main -> //Malterlib/Dev
    [Integrate]
    //Malterlib/Hansoft/X/Main -> //Malterlib/Main (integrated)
    //Malterlib/Hansoft/X/Dev -> //Malterlib/Hansoft/X/Main (integrated)
    
    [Malterlib/Cloud]
    Allow all users to access root directry for app (integrated)
    Fix permissions for application updates (integrated)
    Make sure encryption is opened when updating application (integrated)
    App manager incorrectly closed encryption if application was already stopped (integrated)
    Expose key precreation to command line for key manager (integrated)
    	(han-18638)
    
    [Malterlib/Concurrency]
    Suggest command or option when encountering incorrect one (integrated)
    
    [Malterlib/String]
    Add fuzzy string matching (integrated)
    
    Copied from Perforce
     Change: 68441

---
+++ Source/Malterlib_String_FuzzyMatch.cpp
@@ -0,0 +1,193 @@
+// Copyright © 2015 Hansoft AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#include <Mib/Core/Core>
+#include "Malterlib_String_FuzzyMatch.h"
+
+namespace NMib
+{
+	namespace NStr
+	{
+		namespace 
+		{
+			class CFuzzyMatching
+			{
+			public:
+				CFuzzyMatching()
+				{
+					m_MatichID = 0;
+					m_nMatched = 0;
+				}
+				aint m_MatichID;
+				aint m_nMatched;
+			};
+			
+			template <bint tf_bNoCase, bint tf_bCheckLen, typename tf_CData1, typename tf_CData2>
+			aint fg_StrFindPartial(const tf_CData1 *_pStr1, const tf_CData2 *_pStr2, mint _Len, mint &_Found)
+			{
+				typedef typename NTraits::TCUnsigned<tf_CData1>::CType CData1;
+				typedef typename NTraits::TCUnsigned<tf_CData2>::CType CData2;
+				const CData1 *pStr1Start = (const CData1 *)_pStr1;
+				const CData1 *pStr1 = pStr1Start;
+				const CData1 *pStr1End = pStr1Start + _Len;
+				const CData2 *pStr2 = (const CData2 *)_pStr2;
+
+				while (*pStr1)
+				{
+					if (tf_bCheckLen)
+					{
+						if (pStr1 >= pStr1End)
+						{
+							_Found = 0;
+							return -1;
+						}
+					}
+
+					const CData1 *pStr1Current = pStr1;
+					const CData2 *pStr2Current = pStr2;
+					while (1)
+					{
+						CData1 Data1;
+						CData2 Data2;
+						if (tf_bNoCase)
+						{
+							Data1 = fg_CharLowerCase(*pStr1Current);
+							Data2 = fg_CharLowerCase(*pStr2Current);
+						}
+						else
+						{
+							Data1 = *pStr1Current;
+							Data2 = *pStr2Current;
+						}
+						if (Data1 != Data2)
+						{
+							if (pStr2Current != pStr2)
+							{
+								_Found = pStr2Current - pStr2 ;
+								return pStr1 - pStr1Start;
+							}
+							break;
+						}
+						++pStr1Current;
+						++pStr2Current;
+						if (!(*pStr2Current))
+						{
+							_Found = pStr2Current - pStr2;
+							return pStr1 - pStr1Start;
+						}
+						if (tf_bCheckLen)
+						{
+							if (pStr1Current >= pStr1End)
+							{
+								if (pStr2Current != pStr2)
+								{
+									_Found = pStr2Current - pStr2;
+									return pStr1 - pStr1Start;
+								}
+								_Found = 0;
+								return -1;
+							}
+						}
+						if (!(*pStr1Current))
+						{
+							if (pStr2Current != pStr2)
+							{
+								_Found = pStr2Current - pStr2;
+								return pStr1 - pStr1Start;
+							}
+							_Found = 0;
+							return -1;
+						}
+					}
+
+					++pStr1;
+				}
+				_Found = 0;
+				return -1;
+			}
+		}
+
+		fp64 fg_FuzzyMatchString(CStr _Str0, CStr _Str1)
+		{
+			if (_Str1.f_GetLen() > _Str0.f_GetLen())
+				fg_Swap(_Str0, _Str1);
+
+			aint MaxLen = _Str0.f_GetLen();
+			
+			NContainer::TCVector<CFuzzyMatching> Matchings;
+			Matchings.f_SetLen(MaxLen);
+			CFuzzyMatching *pMatchings = Matchings.f_GetArray();
+			aint iMatingID = 0;
+			
+			const ch8 *pParse0Start = _Str0;
+			const ch8 *pParse1Start = _Str1;
+			const ch8 *pParse1 = pParse1Start;
+			while (*pParse1)
+			{
+				const ch8 *pParse0 = pParse0Start;
+
+				mint Found;
+				aint iFind = fg_StrFindPartial<true, false>(pParse0, pParse1, 0, Found);
+				while (iFind >= 0 && Found >= 3)
+				{
+					++iMatingID;
+					aint iStart = (pParse0 - pParse0Start) + iFind;
+					bint bLarger = true;
+					for (mint i = 0; i < Found; ++i)
+					{
+						if (aint(Found) < pMatchings[iStart + i].m_nMatched)
+						{
+							bLarger = false;
+							break;
+						}
+					}
+					if (bLarger)
+					{
+						// Clear pMatchings
+						aint MatchingID = pMatchings[iStart].m_MatichID;
+						for (aint i = iStart - 1; i >= 0; --i)
+						{
+							if (pMatchings[i].m_MatichID != MatchingID)
+								break;
+							pMatchings[i].m_MatichID = 0;
+							pMatchings[i].m_nMatched = 0;
+						}
+						MatchingID = pMatchings[iStart + Found - 1].m_MatichID;
+						for (aint i = iStart + Found; i < MaxLen; ++i)
+						{
+							if (pMatchings[i].m_MatichID != MatchingID)
+								break;
+							pMatchings[i].m_MatichID = 0;
+							pMatchings[i].m_nMatched = 0;
+						}
+						// Add Mapping
+						for (aint i = iStart; i < aint(iStart + Found); ++i)
+						{
+							pMatchings[i].m_MatichID = iMatingID;
+							pMatchings[i].m_nMatched = Found;
+						}
+					}
+					pParse0 += iFind + 1;
+					iFind = fg_StrFindPartial<true, false>(pParse0, pParse1, 0, Found);
+				}
+				++pParse1;
+			}
+
+			int64 Ret = 0;
+			aint nUnmatched = 0;
+			aint MaxMatch = 3;
+			for (aint i = 0; i < MaxLen; ++i)
+			{
+				if (pMatchings[i].m_nMatched == 0)
+					++nUnmatched;
+				if (pMatchings[i].m_nMatched > MaxMatch)
+					MaxMatch = pMatchings[i].m_nMatched;
+				
+			}
+			for (aint i = 0; i < MaxLen; ++i)
+				Ret += fg_Min(MaxLen - pMatchings[i].m_nMatched, nUnmatched);
+
+			return fp64(Ret) / fp64(MaxLen).f_Sqr() * 1.0 + (fp64(nUnmatched) / fp64(MaxLen)) * 0.0;
+		}
+	}
+}
---
+++ Source/Malterlib_String_FuzzyMatch.h
@@ -0,0 +1,12 @@
+// Copyright © 2015 Hansoft AB 
+// Distributed under the MIT license, see license text in LICENSE.Malterlib
+
+#pragma once
+
+namespace NMib
+{
+	namespace NStr
+	{
+		fp64 fg_FuzzyMatchString(CStr _Str0, CStr _Str1);
+	}
+}
