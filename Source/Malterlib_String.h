6520840f75e21d220305bccfbeacc15bd3d3ed5d
Erik Olofsson <erik@olofsson.info>
2025-05-18 02:07:23 +0200

    [Malterlib/String] Upgrade to llvm 20

--- Source/Malterlib_String.h
+++ Source/Malterlib_String.h
@@ -131,12 +131,12 @@ namespace NMib::NStr
 		return CStr::CFormat(_pStr);
 	}
 
-	inline_always CWStr::CFormat operator "" _f (const ch16 *_pStr, std::size_t _Len)
+	inline_always CWStr::CFormat operator ""_f (const ch16 *_pStr, std::size_t _Len)
 	{
 		return CWStr::CFormat(_pStr);
 	}
 
-	inline_always CUStr::CFormat operator "" _f (const ch32 *_pStr, std::size_t _Len)
+	inline_always CUStr::CFormat operator ""_f (const ch32 *_pStr, std::size_t _Len)
 	{
 		return CUStr::CFormat(_pStr);
 	}
